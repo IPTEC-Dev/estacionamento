ccc9afdb709c65151e5a205f47b4125f0e3d59d9
Anna Ravaglio <55036752+annaravaglio@users.noreply.github.com>
2022-06-15 15:49:52 -0300

    Correção de impressão
    Alterado comando para impressão

--- electron/main.js
+++ electron/main.js
@@ -336,5 +336,6 @@ app.printUsingElectron = function (data, modulo) {
     printWindow.loadURL("C:\\Estacionamento-win32-x64\\result.txt");
   }
 
-  exec("copy /B C:\\Estacionamento-win32-x64\\result.txt LPT1");
+  //exec("copy /B C:\\Estacionamento-win32-x64\\result.txt LPT1");
+  exec("print /d:RECIBOS C:\\Estacionamento-win32-x64\\result.txt");
 };
